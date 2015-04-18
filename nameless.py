f244a3f935aab6a62517aedb119e13f72c6a6cfb
Philip Bedward Jr <philipbed813@gmail.com>
2015-04-18 15:19:18 -0400

    real merge conflict; reply sms messages

--- nameless.py
+++ nameless.py
@@ -1,4 +1,23 @@
+from Flask import Flask, request, redirect
+import twilio.twiml
 __author__ = 'Phil'
 __author__ = 'Rachael'
-r = 5
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond and greet the caller by name."""
+
+    from_number = request.values.get('From', None)
+    message = "At a hackathon testing "
+
+    resp = twilio.twiml.Response()
+    resp.message(message)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
