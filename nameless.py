3089d08af74b1e9a7f8d97f7994bb4755cfd1f34
rachaelthormann <rachaelthormann@gmail.com>
2015-04-18 15:18:23 -0400

    added client

--- nameless.py
+++ nameless.py
@@ -1,4 +1,11 @@
+# Download the twilio-python library from http://twilio.com/docs/libraries
+from twilio.rest import TwilioRestClient
+
 __author__ = 'Phil'
 __author__ = 'Rachael'
-r = 5
+
+# Find these values at https://twilio.com/user/account
+account_sid = "ACb63800b54ab0a07ff095d2632e9755aa"
+auth_token = "8ec0dc3798983cdb5888bdaa511cf958"
+client = TwilioRestClient(account_sid, auth_token)
 
